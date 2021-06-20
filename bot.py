062984e2960a72cf62988b9734f3c946748dbcb1
Nymfeparakit <maxlex047@gmail.com>
2021-06-20 12:08:32 +0300

    refactor: Получение id книг с полкивынесено в отдельную функцию

--- bot.py
+++ bot.py
@@ -32,26 +32,30 @@ async def process_start_command(message: types.Message):
     loop.call_later(SEARCH_DELAY, repeat, process_novelties, loop, message.from_user.id)
 
 
-async def process_novelties(user_id):
-    await bot.send_message(user_id, "Начинаю искать книги")
-    # получаем полку "Куплю"
-    response_text = requests.get(f'{FANTLAB_API_URL}user/175721/bookcases').text
-    shelves = json.loads(response_text)
-    buy_shelve = list(filter(lambda shelve: shelve['bookcase_name'] == 'Куплю', shelves))[0]
-    buy_shelve_id = buy_shelve['bookcase_id']
-
-    # получаем id всех изданий с полки "куплю"
+def get_books_ids_from_shelf(shelve_id):
     shelve_books_ids = []
     offset = 0
-    # TODO: возможно здесь можно сделать генератор
     while True:
         response_text = requests.get(f'{FANTLAB_API_URL}user/175721/bookcase/{buy_shelve_id}?offset={offset}').text
         print(f'{FANTLAB_API_URL}user/175721/bookcase/{buy_shelve_id}')
         shelve_books = json.loads(response_text)['bookcase_items']
         shelve_books_ids.extend([item['edition_id'] for item in shelve_books])
-        if not shelve_books:
-            break
+        if not shelve_books: # если больше нет книг по запросу
+            return shelve_books_ids
         offset += 10
+
+
+async def process_novelties(user_id):
+    await bot.send_message(user_id, "Начинаю искать книги")
+    # получаем полку "Куплю"
+    response_text = requests.get(f'{FANTLAB_API_URL}user/175721/bookcases').text
+    shelves = json.loads(response_text)
+    # TODO: исправить shelve -> shelf
+    buy_shelve = list(filter(lambda shelve: shelve['bookcase_name'] == 'Куплю', shelves))[0]
+    buy_shelve_id = buy_shelve['bookcase_id']
+
+    # получаем id всех изданий с полки "куплю"
+    shelve_books_ids = get_books_ids_from_shelf(buy_shelve_id)
 
     # получаем сведения о новинках
     response_text = requests.get(f'{FANTLAB_API_URL}pubnews').text
